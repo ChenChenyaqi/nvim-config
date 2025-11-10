6111d07976908f2bc427ae40b41b7b3d9c7cfa2e
chenyaqi <15229293783@163.com>
2025-11-10 23:28:53 +0800

    chapter 2

--- init.lua
+++ init.lua
@@ -1,8 +1,24 @@
+-- disable netrw at the very start of your init.lua
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 vim.opt.number=true
 vim.wo.cursorline = true
 -- Display tabs and trailing spaces
 vim.opt.list = true
 vim.opt.listchars = { tab = ">-", trail = "-" }
+
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+vim.opt.hlsearch = true
+
+vim.opt.scrolloff = 15
+vim.opt.sidescrolloff = 10
+vim.opt.startofline = false
+
+vim.opt.conceallevel = 2
+
+vim.wo.wrap = false
 
 -- Tab related options
 vim.opt.softtabstop = 2
@@ -16,6 +32,7 @@ vim.opt.smartindent = true
 vim.g.mapleader = " "
 vim.g.maplocalleader = "\\"
 
-
 require("config.lazy") -- Import `./lua/config/lazy.lua`
+
+require("keymapping")
 
