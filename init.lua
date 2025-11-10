cd9fe5d69b6753ae40a1643fa3af86bf8a04c7c3
chenyaqi <15229293783@163.com>
2025-11-10 23:20:22 +0800

    chapter 1

--- init.lua
+++ init.lua
@@ -1,1 +1,21 @@
-require("config.lazy")
+vim.opt.number=true
+vim.wo.cursorline = true
+-- Display tabs and trailing spaces
+vim.opt.list = true
+vim.opt.listchars = { tab = ">-", trail = "-" }
+
+-- Tab related options
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+vim.opt.smartindent = true
+
+-- Make sure to setup `mapleader` and `maplocalleader` before
+-- loading lazy.nvim so that mappings are correct.
+-- This is also a good place to setup other settings (vim.opt)
+vim.g.mapleader = " "
+vim.g.maplocalleader = "\\"
+
+
+require("config.lazy") -- Import `./lua/config/lazy.lua`
+
