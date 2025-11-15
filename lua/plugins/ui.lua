fe50a3ab906ee113388bf37a4e2074e5b413ed88
chenyaqi <15229293783@163.com>
2025-11-15 11:22:40 +0800

    fix: 会话管理，yazi文件管理器

--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -537,4 +537,25 @@ return {
       vim.keymap.set("n", "zX", "<NOP>", { desc = "Disabled" })
     end,
   },
+
+  -- yazi文件管理器
+  {
+    "mikavilpas/yazi.nvim",
+    dependencies = { "folke/snacks.nvim" },
+    -- stylua: ignore
+    keys = {
+      { "<leader>E",  "<CMD>Yazi<CR>",        desc = "[Yazi] open at the current file", mode = { "n", "v" } },
+      { "<leader>cw", "<CMD>Yazi cwd<CR>",    desc = "[Yazi] open in working directory"                     },
+      { "<c-up>",     "<CMD>Yazi toggle<CR>", desc = "[Yazi] Resume the last session"                       },
+    },
+    opts = {
+      open_for_directories = false,
+      keymaps = {
+        show_help = "<f1>",
+      },
+    },
+    init = function()
+      vim.g.loaded_netrwPlugin = 1
+    end,
+  },
 }
